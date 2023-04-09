3db1d1c1bcf70683552d8dee9c2a3502b0ef1575
Aleksandra Altaeva <svvvlt0310@gmail.com>
2023-04-09 22:48:25 +0300

    task54

---
+++ task54/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу,
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int rows = InPut("Введите количество строк: ");
+int cols = InPut("Введите количество столбцов: ");
+int[,] mass = FillArray(rows, cols, 0, 10);
+PrintArray(mass);
+System.Console.WriteLine();
+PrintArray(SortArray(mass));
+
+//---------заполнение массива--------
+int[,] FillArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+//--------вывод массива---------
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} "); ;
+        }
+        Console.WriteLine();
+    }
+}
+
+//-------приглашение ко вводу числа-----
+int InPut(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+//-------сортировка элементов строки по убыванию-----
+int[,] SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, j])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
